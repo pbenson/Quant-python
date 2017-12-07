d7ad5449c9037d28796485f641a83d915c160311
pbenson <pbenson@me.com>
2017-12-07 10:51:35 -0500

    Swapped black and red, so that "in the black/red" means picking black means winning $1, picking  red loses $1

--- RedBlackMarbles/main.py
+++ RedBlackMarbles/main.py
@@ -1,14 +1,14 @@
 class BagNode:
-    def __init__(self, red, black, bag_nodes):
-        self.red = red
+    def __init__(self, black, red, bag_nodes):
         self.black = black
+        self.red = red
         self.bag_nodes = bag_nodes
-        self.stop = False
-        if red == 0:
-            self.policy_winnings = black
+        if black == 0:
             self.stop = True
+            self.policy_winnings = red
         else:
-            if black == 0:
+            if red == 0:
+                self.stop = False
                 self.policy_winnings = 0
             else:
                 self.policy_winnings = None
@@ -21,21 +21,27 @@ class BagNode:
             winnings_if_go = 0
             if self.red > 0:
                 winnings_if_go += float(self.red) / (self.red + self.black) \
-                                  * self.bag_nodes.bag_node(self.red - 1, self.black).expected_winnings()
+                                  * self.bag_nodes.bag_node(self.black, self.red - 1).expected_winnings()
             if self.black > 0:
                 winnings_if_go += float(self.black) / (self.red + self.black) \
-                                  * self.bag_nodes.bag_node(self.red, self.black - 1).expected_winnings()
-            self.stop = winnings_if_go < self.black - self.red
+                                  * self.bag_nodes.bag_node(self.black  - 1, self.red).expected_winnings()
+            winnings_if_stop = self.red - self.black
+            self.stop = winnings_if_go < winnings_if_stop
             if self.stop:
-                self.policy_winnings = self.black - self.red
+                self.policy_winnings = winnings_if_stop
             else:
                 self.policy_winnings = winnings_if_go
         return self.policy_winnings
+
+    def stage(self):
+        return self.red + self.black
 
 
 class BagNodes:
     def __init__(self):
         self.red_black_pair_to_bag_dict = {}
+        # the fewer red remaining in bag
+        self.min_red_for_stop_by_stage = {}
 
     def bag_node(self, red, black):
         key = (red, black)
@@ -45,5 +51,5 @@ class BagNodes:
 
 
 bag_nodes = BagNodes()
-x = bag_nodes.bag_node(26, 17).expected_winnings()
+x = bag_nodes.bag_node(250, 250).expected_winnings()
 print(x)
