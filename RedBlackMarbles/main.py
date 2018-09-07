d67a1187c6cd4cee69b9d0636225e077c58ba996
pbenson <pbenson@me.com>
2018-09-07 07:17:08 -0400

    class renaming

--- RedBlackMarbles/main.py
+++ RedBlackMarbles/main.py
@@ -1,8 +1,8 @@
-class BagNode:
-    def __init__(self, black, red, bag_nodes):
+class DPNode:
+    def __init__(self, black, red, dp):
         self.black = black
         self.red = red
-        self.bag_nodes = bag_nodes
+        self.dp = dp
         if black == 0:
             self.stop = True
             self.policy_winnings = red
@@ -20,11 +20,11 @@ class BagNode:
         if not self.policy_winnings:
             winnings_if_go = 0
             if self.red > 0:
-                winnings_if_go += float(self.red) / (self.red + self.black) \
-                                  * self.bag_nodes.bag_node(self.black, self.red - 1).expected_winnings()
+                winnings_if_go += self.red / (self.red + self.black) \
+                                  * self.dp.node(self.black, self.red - 1).expected_winnings()
             if self.black > 0:
-                winnings_if_go += float(self.black) / (self.red + self.black) \
-                                  * self.bag_nodes.bag_node(self.black  - 1, self.red).expected_winnings()
+                winnings_if_go += self.black / (self.red + self.black) \
+                                  * self.dp.node(self.black  - 1, self.red).expected_winnings()
             winnings_if_stop = self.red - self.black
             self.stop = winnings_if_go < winnings_if_stop
             if self.stop:
@@ -37,19 +37,19 @@ class BagNode:
         return self.red + self.black
 
 
-class BagNodes:
+class DP:
     def __init__(self):
-        self.red_black_pair_to_bag_dict = {}
+        self.red_black_pair_to_node_dict = {}
         # the fewer red remaining in bag
         self.min_red_for_stop_by_stage = {}
 
-    def bag_node(self, red, black):
+    def node(self, red, black):
         key = (red, black)
-        if key not in self.red_black_pair_to_bag_dict:
-            self.red_black_pair_to_bag_dict[key] = BagNode(red, black, self)
-        return self.red_black_pair_to_bag_dict[key]
+        if key not in self.red_black_pair_to_node_dict:
+            self.red_black_pair_to_node_dict[key] = DPNode(red, black, self)
+        return self.red_black_pair_to_node_dict[key]
 
 
-bag_nodes = BagNodes()
-x = bag_nodes.bag_node(1000, 1000).expected_winnings()
+dp = DP()
+x = dp.node(30, 30).expected_winnings()
 print(x)
